70c0dc658619e7e8766bbc107f6e141f6e0b5643
mudunuri-lakshmi-harika <130353775+mudunuri-lakshmi-harika@users.noreply.github.com>
2025-03-25 09:15:11 +0000

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,18 @@
+const CartItems=document.querySelector(".cart-items");
+function DisplayCartItems(){
+  const items=JSON.parse(localStorage.getItem("cart"));
+  items.forEach(item => {
+    const cartItem=document.createElement("div");
+    cartItem.className="cart_item";
+    cartItem.innerHTML=`
+    <div class="cart_item">
+              <p class="cart_id" >${item.id}</p>
+              <p class="cart_title">${item.title}</p>
+              <img src="${item.images}" alt="${item.title}" class="cart_img"/>
+              <p class="cart_price">${item.price}</p>
+              <p class="cart_delete">Delete</p>
+    `
+    CartItems.appendChild(cartItem);
+  });
+}
+DisplayCartItems();
